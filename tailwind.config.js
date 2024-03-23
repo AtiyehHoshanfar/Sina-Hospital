c8ae55885122c90496a564023f690d34413e16e4
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-03-23 22:49:54 +0330

    initialize project

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      vazir: ["Vazirmatn", "sans-serif"],
+    },
+
+    extend: {
+      colors: {
+        Teal: {
+          50: '#FFFFFF',
+          100: '#D5F5F6',
+          200: '#AFECEF',
+          300: '#7EDCE2',
+          400: '#16BDCA',
+          500: '#0694A2',
+          600: '#047481',
+          700: '#036672',
+          800: '#05505C',
+          900: '#014451',
+        },
+      }
+    },
+  },
+  plugins: [],
+};
