340419ddf25a3aaf90b28d7e93cfd6478ff42d89
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-03-27 14:41:39 +0330

    add sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,28 @@
+// ? TYPE WRITER
 // !Selector
-const title=document.querySelector(".title")
-// ! TypeWriter
+const title = document.querySelector(".title");
+
 let typewriter = new Typewriter(title, {
-    loop: false
+  loop: false,
 });
 
-typewriter.typeString('به وبسایت مرکز تحقیقات ام اس دانشگاه ')
-.typeString('<span class=" text-Teal-500">علوم پزشکی تهران </span>')
-.typeString('خوش آمدید')
-    .start();
-// !Functions
+typewriter
+  .typeString("به وبسایت مرکز تحقیقات ام اس دانشگاه ")
+  .typeString('<span class=" text-Teal-500">علوم پزشکی تهران </span>')
+  .typeString("خوش آمدید")
+  .start();
 
-// !Event Listener
-
+// ? HAMBURGER MENU
+// !Selector
+const hamburgerButton = document.querySelector("#hamburger-button");
+const sidebar = document.querySelector("#sidebar");
+// ! Event Listener
+document.addEventListener("click", (e) => {
+  if (e.target.id === hamburgerButton.id) sidebar.classList.toggle("isOpen");
+  else if (
+    e.target.parentElement.id === sidebar.id ||
+    e.target.parentElement.parentElement.id === sidebar.id
+  )
+    sidebar.classList.add("isOpen");
+  else if (e.target.id !== sidebar.id) sidebar.classList.remove("isOpen");
+});
