642c5bdcddfccfdcc5aee01f7b55c93b36df546a
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-04-07 23:57:29 +0330

    adding layout for about doctor page

---
+++ src/js/sidebar.js
@@ -0,0 +1,13 @@
+// !Selector
+const hamburgerButton = document.querySelector("#hamburger-button");
+const sidebar = document.querySelector("#sidebar");
+// ! Event Listener
+document.addEventListener("click", (e) => {
+  if (e.target.id === hamburgerButton.id) sidebar.classList.toggle("isOpen");
+  else if (
+    e.target.parentElement.id === sidebar.id ||
+    e.target.parentElement.parentElement.id === sidebar.id
+  )
+    sidebar.classList.add("isOpen");
+  else if (e.target.id !== sidebar.id) sidebar.classList.remove("isOpen");
+});
