80976224048a079e8c3106f00d83bb0a3d80412c
AtiyehHoshanfar <atiyeh.hoshanfar@gmail.com>
2024-04-10 16:27:33 +0330

    refactor change accordion code

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -1,24 +1,20 @@
-
-const accordion={
-    openId:null
-}
+const contents=document.querySelectorAll(".content")
 const tabs=document.querySelectorAll(".tab")
 tabs.forEach(tab=>tab.addEventListener("click",(e)=>{
-    accordion.openId=tab.dataset.id
-
-    tabs.forEach(tab=>{
-        console.log(tab)
-        if(tab.dataset.id===accordion.openId){
+    closeAllTabs()
+    const content=tab.querySelector(".content")
             tab.classList.remove("close")
-            tab.classList.add("open")       
-        }
-        else{
-            tab.classList.remove("open")
-            tab.classList.add("close")
-
-        }
-    }
-
-    )
+            tab.classList.add("open")    
+    content.style.height=content.scrollHeight+"px"
 }))
 
+
+function closeAllTabs(){
+contents.forEach(content=>{
+    content.style.height="0px"})
+    tabs.forEach(tab=>{
+            tab.classList.remove("open")
+            tab.classList.add("close")
+    })
+    
+}
